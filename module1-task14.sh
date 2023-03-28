b139a7474b65e0ba96ce023da33d548c31c2dd98
neudaryu <104096827+neudaryu@users.noreply.github.com>
2023-03-28 16:34:04 +0300

    Update module1-task14.sh

--- module1-task14.sh
+++ module1-task14.sh
@@ -12,9 +12,9 @@ do
 done
 
 #copy files from the local system to path $REMOTE_DIR on $REMOTE_HOST using $REMOTE_USER credentials 
-rsync -avz /var/log/file?.txt "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR"; 
+rsync -avz /var/log/file?.txt "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR" 
 
 #connect to remote machine using ssh,search for files in the $REMOTE_DIR that were modified more than 7 days ago & delete it
-ssh "$REMOTE_USER@$REMOTE_HOST" find "REMOTE_DIR" type -f -name "*.log" -mtime +7 -exec rm {} \;
+ssh "$REMOTE_USER@$REMOTE_HOST" find "REMOTE_DIR" type -f -name "*.log" -mtime +7 -exec rm {} \
 
 #0 0 * * * root /home/user/script.sh - runs script every day at 00:00
